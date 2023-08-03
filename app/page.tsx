3edcc3d1a2094d34287c9b437651066d60500e62
micainteriors <interiorsbymica@gmail.com>
2023-08-03 00:53:11 -0700

    Listings Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import Image from 'next/image'
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
 
 export default function Home() {
   return (
    <>
-   <div className="">
-    Hello ding dong
-   </div>
+   <ClientOnly >
+    <Container>
+       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>Featured Listings Goes here </div>
+          <div>Featured Listings2 Goes here </div>
+          <div>Featured Listings3 Goes here </div>
+          <div>Featured Listings4 Goes here </div>
+          <div>Featured Listings5 Goes here </div>
+          <div>Featured Listings6 Goes here </div>
+
+       </div>
+    </Container>
+   </ClientOnly>
    </>
   )
 }
