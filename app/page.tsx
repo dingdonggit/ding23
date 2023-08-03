1e4daab9c729fbaa876bf3fb4ddef3a5a775a550
micainteriors <interiorsbymica@gmail.com>
2023-08-03 01:01:28 -0700

    fix padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
    <>
    <ClientOnly >
     <Container>
-       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+       <div className="pt-32 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           <div>Featured Listings Goes here </div>
           <div>Featured Listings2 Goes here </div>
           <div>Featured Listings3 Goes here </div>
