ea34ccc1771b5acf692015614f5d4ee05fc77a20
micainteriors <interiorsbymica@gmail.com>
2023-08-09 08:58:43 -0700

    header padding

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar: React.FC<NavbarProps> = ({
   currentUser,
 }) => {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
+    <div className="fixed w-full bg-white z-10 shadow-sm px-10">
     <div className="py-4 border-b-[1px]">
         <Container>
           <div className=" flex flex-row items-center justify-between gap-3 md:gap-0">
